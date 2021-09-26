1105b59cb2eb99d85d3b100cfebd8a1653588c9e
Olaolu Olowe <olaoluolowe@gmail.com>
2021-09-26 20:04:11 +0100

    StringsAreCool

--- My First Project/src/StringsAreCool.java
+++ My First Project/src/StringsAreCool.java
@@ -1,2 +1,27 @@
 public class StringsAreCool {
+    public static void main(String[] args) {
+        String codenific = "Codenific" ;
+        System.out.println("1. "+ codenific);
+
+        codenific += ".com" ;
+        System.out.println( "2. "+ codenific);
+
+
+        String onlyCodenific = codenific.substring(0, 9);
+        System.out.println("3. "+ onlyCodenific);
+
+        String onlyDotCom =  codenific.substring(9, 13);
+        System . out . println( "4." + onlyDotCom);
+
+
+        codenific = " " + codenific + " " ;
+        codenific = codenific. trim();
+        System.out.println("5. "+codenific);
+
+        System . out . println( "6. " + codenific. toUpperCase());
+        System . out . println( "7. " + codenific. toLowerCase());
+
+
+
+    }
 }
